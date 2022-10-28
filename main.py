e5a89dbeb966451a1cd14b20b1a7972aa99be994
ebrooks <evelynb92@hotmail.com>
2022-10-28 09:35:24 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+from typing import Any
+
+
+def getword():
+    word_list = ['air', 'apple', 'baby', 'birth', 'cheese', 'connection', 'distribution', 'electric', 'exchange',
+                 'feather', 'flower', 'grain', 'gun', 'hanging', 'harbour', 'insurance', 'island', 'journey', 'kettle',
+                 'learning', 'liquid', 'material']
+
+    word_index = random.choice(word_list)
+    return word_index
+
