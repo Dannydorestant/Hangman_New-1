befcded78d3068a3ecbd42ff1475b5b3c2386a85
daniel <dandorestant@gmail.com>
2022-10-31 01:16:17 -0400

    v3

--- hangman.py
+++ hangman.py
@@ -1,17 +1,12 @@
 import random
 
-
 def getword():
     word_list = ['air', 'apple', 'baby', 'birth', 'cheese', 'connection', 'distribution', 'electric', 'exchange',
                  'feather', 'flower', 'grain', 'gun', 'hanging', 'harbour', 'insurance', 'island', 'journey', 'kettle',
                  'learning', 'liquid', 'material']
 
     word_index = random.choice(word_list)
-    len_word = len(word_index)
-    print(word_index,len_word)
-
-getword()
-
+    return(word_index)
 
 
 
--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for
+from hangman import getword
 
 app = Flask(__name__)
 
-@app.route('/')
+global guesscount
+guesscount = 6
+
+guesses = []
+
+global word
+word = getword()
+
+global wordletters
+wordletters = [letter for letter in word]
+
+global lettercount
+lettercount = len(word)
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('hangman.html')
+    global guesscount
+    if request.method == "POST":
+        guess = request.form['User-input']
+        if not guess in guesses:
+            guesses.append(guess)
+            print(guesses)
+        if not guess in wordletters:
+            guesscount -= 1
+        displayimage = f"static/imgs/Hangman0{guesscount}.png"
+        return render_template('hangman.html', guesscount=guesscount, displayimage=displayimage, word=word, guesses=guesses)
+    else:
+        displayimage = f"static/imgs/Hangman0{guesscount}.png"
+        return render_template('hangman.html', guesscount=guesscount, displayimage=displayimage, word=word)
 
-app.route('/', methods=['POST'])
-
-def get_guess():
-    guess = request.form['User-input']
-    print(guess)
 
 if __name__ == "__main__":
-    app.run(host="192.168.1.76")
-
-
-get_guess()
+    app.run()
